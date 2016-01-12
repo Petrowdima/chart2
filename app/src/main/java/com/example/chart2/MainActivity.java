5a8363b37e3e5f1713b4f38b75eb0c499970e8f2
Petrowdima <alekseyf2016@yandex.ru>
2016-01-12 16:11:04 +0200

    Initial commit

---
+++ app/src/main/java/com/example/chart2/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.chart2;
+
+
+        import android.app.Activity;
+        import android.os.Bundle;
+        import android.widget.LinearLayout;
+
+        import org.achartengine.ChartFactory;
+        import org.achartengine.GraphicalView;
+        import org.achartengine.model.XYMultipleSeriesDataset;
+        import org.achartengine.model.XYSeries;
+        import org.achartengine.renderer.XYMultipleSeriesRenderer;
+        import org.achartengine.renderer.XYSeriesRenderer;
+
+public class MainActivity extends Activity {
+
+    private GraphicalView mChart;
+    private XYMultipleSeriesDataset mDataset = new XYMultipleSeriesDataset();
+    private XYMultipleSeriesRenderer mRenderer = new XYMultipleSeriesRenderer();
+    private XYSeries mCurrentSeries;
+    private XYSeriesRenderer mCurrentRenderer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        // TODO Auto-generated method stub
+        super.onResume();
+        LinearLayout layout = (LinearLayout) findViewById(R.id.LinearLayout);
+        if (mChart == null) {
+            initChart();
+            addSampleData();
+            mChart = ChartFactory.getCubeLineChartView(this, mDataset, mRenderer, 0.3f);
+            layout.addView(mChart);
+        } else {
+            mChart.repaint();
+        }
+    }
+
+    private void initChart() {
+        mCurrentSeries = new XYSeries("Sample Data");
+        mDataset.addSeries(mCurrentSeries);
+        mCurrentRenderer = new XYSeriesRenderer();
+        mRenderer.addSeriesRenderer(mCurrentRenderer);
+    }
+
+    private void addSampleData() {
+        mCurrentSeries.add(1, 2);
+        mCurrentSeries.add(2, 3);
+        mCurrentSeries.add(3, 2);
+        mCurrentSeries.add(4, 5);
+        mCurrentSeries.add(5, 4);
+    }
+}
